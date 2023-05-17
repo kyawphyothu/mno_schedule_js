1766edafdf9cf8b3403d883d0bdaacb7f6aacdf3
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-05-17 20:27:58 +0630

    publicHoliday file location change and remove 6:30a bug with toISOstring()

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { publicHoliday } from "./publicHoliday.js";
+import { publicHoliday } from "./publicHoliday/publicHoliday.js";
 import { isCookieSet, getCookie } from "./checkCookies.js";
 
 const eventDates = []; // Define an array to store the event dates
@@ -40,9 +40,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 	// Define the duration of each segment in milliseconds
-	const dayShiftDuration = 1 * 24 * 60 * 60 * 1000; // 2 days
-	const restDuration = 1 * 24 * 60 * 60 * 1000; // 2 days
-	const nightShiftDuration = 1 * 24 * 60 * 60 * 1000; // 2 days
+	const dayShiftDuration = 2 * 24 * 60 * 60 * 1000; // 2 days
+	const restDuration = 2 * 24 * 60 * 60 * 1000; // 2 days
+	const nightShiftDuration = 2 * 24 * 60 * 60 * 1000; // 2 days
 
 	const separateDay = 1 * 24 * 60 * 60 * 1000; // 1 days
 
@@ -60,36 +60,36 @@ document.addEventListener('DOMContentLoaded', function() {
         // Calculate the start and end dates of each segment based on the current cycle
         const dayShiftStart = new Date(startDate.getTime() + i * totalDuration);
         const dayShiftEnd = new Date(dayShiftStart.getTime() + dayShiftDuration);
-        const dayRestStart = new Date(dayShiftEnd.getTime() + separateDay);
+        const dayRestStart = new Date(dayShiftEnd.getTime() );
         const dayRestEnd = new Date(dayRestStart.getTime() + restDuration);
-        const nightShiftStart = new Date(dayRestEnd.getTime() + separateDay);
+        const nightShiftStart = new Date(dayRestEnd.getTime() );
         const nightShiftEnd = new Date(nightShiftStart.getTime() + nightShiftDuration);
-        const nightRestStart = new Date(nightShiftEnd.getTime() + separateDay);
+        const nightRestStart = new Date(nightShiftEnd.getTime() );
         const nightRestEnd = new Date(nightRestStart.getTime() + restDuration);
 
         // Add the segment dates to the eventDates array
         eventDates.push({
             title: 'DayShift',
-            start: dayShiftStart,
-            end: dayShiftEnd,
+            start: dayShiftStart.toISOString().split('T')[0],
+            end: dayShiftEnd.toISOString().split('T')[0],
             color: colors[0],
         });
         eventDates.push({
             title: 'OFF',
-            start: dayRestStart,
-            end: dayRestEnd,
+            start: dayRestStart.toISOString().split('T')[0],
+            end: dayRestEnd.toISOString().split('T')[0],
             color: colors[1],
         });
         eventDates.push({
             title: 'NightShift',
-            start: nightShiftStart,
-            end: nightShiftEnd,
+            start: nightShiftStart.toISOString().split('T')[0],
+            end: nightShiftEnd.toISOString().split('T')[0],
             color: colors[2],
         });
         eventDates.push({
             title: 'OFF',
-            start: nightRestStart,
-            end: nightRestEnd,
+            start: nightRestStart.toISOString().split('T')[0],
+            end: nightRestEnd.toISOString().split('T')[0],
             color: colors[1],
         });
 	}
